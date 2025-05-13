c17a7847d157c33e7e81c1db332842cf53e56f57
cassandracrawford <crfeliciano1989@gmail.com>
2025-05-13 10:02:10 -0600

    Set up for Week 2 Assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+        <p>Cassandra Crawford</p>
+        <p><Link href="https://github.com/cassandracrawford">https://github.com/</Link></p>
+    </div>
+  );
+}
