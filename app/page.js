08a220f16b60ebd26b41842a1e7a730ed2ac60d3
cassandracrawford <crfeliciano1989@gmail.com>
2025-05-14 19:43:16 -0600

    Add correct link

--- app/page.js
+++ app/page.js
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-4xl font-bold">CPRG 306: Web Development 2 - Assignments</h1>
-      <ul>
+    <main className="m-8">
+      <h1 className="text-4xl font-bold bg-yellow-400 p-4">CPRG 306: Web Development 2 - Assignments</h1>
+      <ul className="p-4">
           <li>
-            <Link href="./week-2">Week 2 Assignment</Link>
+            <Link href="./week-2" className="hover:text-amber-900">Week 2 Assignment</Link>
           </li>
       </ul>
     </main>
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,7 +4,7 @@ export default function StudentInfo() {
   return (
     <div>
         <p>Cassandra Crawford</p>
-        <p><Link href="https://github.com/cassandracrawford">https://github.com/</Link></p>
+        <p><Link href="https://github.com/cassandracrawford/cprg306-assignments">https://github.com/</Link></p>
     </div>
   );
 }
