256412026c405a02a6e9b92fcea547deea59f42a
cassandracrawford <crfeliciano1989@gmail.com>
2025-05-14 19:54:01 -0600

    Change text color

--- app/page.js
+++ app/page.js
@@ -3,10 +3,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="m-8">
-      <h1 className="text-4xl font-bold bg-yellow-400 p-4">CPRG 306: Web Development 2 - Assignments</h1>
+      <h1 className="text-4xl text-white font-bold bg-yellow-400 p-4">CPRG 306: Web Development 2 - Assignments</h1>
       <ul className="p-4">
           <li>
-            <Link href="./week-2" className="hover:text-amber-900">Week 2 Assignment</Link>
+            <Link href="./week-2" className="text-amber-900 hover:text-amber-700 hover:underline">Week 2 Assignment</Link>
           </li>
       </ul>
     </main>
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -4,7 +4,7 @@ export default function StudentInfo() {
   return (
     <div>
         <p>Cassandra Crawford</p>
-        <p><Link href="https://github.com/cassandracrawford/cprg306-assignments">https://github.com/</Link></p>
+        <p><Link href="https://github.com/cassandracrawford/cprg306-assignments" className="text-amber-900 hover:text-amber-700 hover:underline">https://github.com/cassandracrawford/cprg306-assignment/</Link></p>
     </div>
   );
 }
