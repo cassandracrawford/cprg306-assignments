3a434ae004a5934f9b705509ee49871ffcbbda6e
cassandracrawford <crfeliciano1989@gmail.com>
2025-05-27 10:34:32 -0600

    Update new-item.js

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -22,8 +22,18 @@ export default function NewItem() {
         <div className="flex flex-col items-center w-full">
             <div className="bg-white w-64 p-6 mt-6 mb-3 rounded-2xl text-center font-bold text-4xl">{quantity}</div>
             <div className="flex items-center gap-4">
-                <button onClick={decrement} disabled={quantity === 1} className={pageStyle + (quantity > 1 ? " cursor-pointer" : "")}>-</button>
-                <button onClick={increment} disabled={quantity === 20} className={pageStyle + (quantity < 20 ? " cursor-pointer" : "")}>+</button>
+                <button 
+                    onClick={decrement} 
+                    disabled={quantity === 1} 
+                    className={pageStyle + (quantity > 1 ? " cursor-pointer" : "")}>
+                        -
+                </button>
+                <button 
+                    onClick={increment} 
+                    disabled={quantity === 20} 
+                    className={pageStyle + (quantity < 20 ? " cursor-pointer" : "")}>
+                        +
+                </button>
             </div>
         </div>
     );
